0447a6b6fda2661cee686f4232cb86c18141d0a3
Ferhat Sobay <ferhat.sobay@yahoo.com.tr>
2013-04-14 00:50:20 +0300

    udp reconnect

--- src/main/java/com/aphyr/riemann/client/UdpTransport.java
+++ src/main/java/com/aphyr/riemann/client/UdpTransport.java
@@ -1,182 +1,237 @@
 package com.aphyr.riemann.client;
 
-import java.net.InetSocketAddress;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.net.*;
-import java.io.*;
-import org.jboss.netty.util.HashedWheelTimer;
-import org.jboss.netty.util.Timer;
-import org.jboss.netty.channel.*;
-import org.jboss.netty.channel.group.*;
-import org.jboss.netty.handler.codec.oneone.*;
-import org.jboss.netty.handler.codec.protobuf.*;
-import org.jboss.netty.handler.codec.frame.*;
-import org.jboss.netty.bootstrap.ConnectionlessBootstrap;
-import org.jboss.netty.handler.execution.ExecutionHandler;
-import org.jboss.netty.handler.execution.OrderedMemoryAwareThreadPoolExecutor;
 import com.aphyr.riemann.Proto.Msg;
-import org.jboss.netty.channel.socket.nio.*;
-import java.util.concurrent.atomic.*;
+import org.jboss.netty.bootstrap.ConnectionlessBootstrap;
+import org.jboss.netty.channel.*;
+import org.jboss.netty.channel.socket.nio.NioDatagramChannelFactory;
+import org.jboss.netty.handler.codec.protobuf.ProtobufEncoder;
+import org.jboss.netty.handler.timeout.WriteTimeoutException;
+import org.jboss.netty.handler.timeout.WriteTimeoutHandler;
+import org.jboss.netty.util.HashedWheelTimer;
+import org.jboss.netty.util.Timeout;
+import org.jboss.netty.util.Timer;
+import org.jboss.netty.util.TimerTask;
+
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class UdpTransport implements SynchronousTransport {
-  // For writes we don't care about
-  public static final Promise<Msg> blackhole = 
-    new Promise<Msg>();
+    // For writes we don't care about
+    public static final Promise<Msg> blackhole =
+            new Promise<Msg>();
 
-  // Shared pipeline handlers
-  public static final ProtobufEncoder pbEncoder = new ProtobufEncoder();
-  public static final DiscardHandler discardHandler = new DiscardHandler();
+    // Shared pipeline handlers
+    public static final ProtobufEncoder pbEncoder = new ProtobufEncoder();
+    public static final DiscardHandler discardHandler = new DiscardHandler();
 
-  public static final int DEFAULT_PORT = 5555;
+    public static final int DEFAULT_PORT = 5555;
 
-  // I AM A STATE MUSHEEN
-  public enum State {
-    DISCONNECTED,
-     CONNECTING,
-      CONNECTED,
-      DISCONNECTING
-  }
+    // Sleep 10 seconds before a reconnection attempt.
+    static final int RECONNECT_DELAY = 10;
 
-  // STATE STATE STATE
-  public volatile State state = State.DISCONNECTED;
-  public volatile Channel channel = null;
-  public volatile ConnectionlessBootstrap bootstrap;
+    // Reconnect when we can not write to server for 10 seconds.
+    private static final int WRITE_TIMEOUT = 10;
 
-  // Configuration
-  public final AtomicLong connectTimeout = new AtomicLong(5000);
-  // Changes to this value are applied only on reconnect.
-  public final AtomicInteger sendBufferSize = new AtomicInteger(16384);
-  public final InetSocketAddress address;
-
-  public UdpTransport(final InetSocketAddress address) {
-    this.address = address;
-  }
-
-  public UdpTransport(final String host, final int port) throws IOException {
-    this(new InetSocketAddress(host, port));
-  }
-
-  public UdpTransport(final String host) throws IOException {
-    this(host, DEFAULT_PORT);
-  }
-
-  public UdpTransport(final int port) throws IOException {
-    this(InetAddress.getLocalHost().getHostAddress(), port);
-  }
-
-  @Override
-  public boolean isConnected() {
-    // Are we in state connected?
-    return state == State.CONNECTED;
-  }
-
-  @Override
-  // Does nothing if not currently disconnected.
-  public synchronized void connect() throws IOException {
-    if (state != State.DISCONNECTED) {
-      return;
-    };
-    state = State.CONNECTING;
-
-    // Set up channel factory
-    final ChannelFactory channelFactory = new NioDatagramChannelFactory(
-        Executors.newCachedThreadPool());
-
-    // Create bootstrap
-    bootstrap = new ConnectionlessBootstrap(channelFactory);
-
-    // Set up pipeline factory.
-    bootstrap.setPipelineFactory(
-        new ChannelPipelineFactory() {
-          public ChannelPipeline getPipeline() {
-            final ChannelPipeline p = Channels.pipeline();
-
-            p.addLast("protobuf-encoder", pbEncoder);
-            p.addLast("discard", discardHandler);
-            
-            return p;
-          }});
-
-    // Set bootstrap options
-    bootstrap.setOption("remoteAddress", address);
-    bootstrap.setOption("sendBufferSize", sendBufferSize.get());
-
-    // Connect
-    final ChannelFuture result = bootstrap.connect().awaitUninterruptibly();
-    
-    // Check for errors.
-    if (! result.isSuccess()) {
-      disconnect(true);
-      throw new IOException("Connection failed", result.getCause());
-    }
-    
-    // Set up channel
-    channel = result.getChannel();
-    channel.setReadable(false);
-
-    // Done
-    state = State.CONNECTED;
-  }
-
-  @Override
-  public void disconnect() throws IOException {
-    disconnect(false);
-  }
-
-  public synchronized void disconnect(boolean force) throws IOException {
-    if (!(force || state == State.CONNECTED)) {
-      return;
+    // I AM A STATE MUSHEEN
+    public enum State {
+        DISCONNECTED,
+        CONNECTING,
+        CONNECTED,
+        DISCONNECTING
     }
 
-    try {
-      channel.close().awaitUninterruptibly();
-      bootstrap.releaseExternalResources();
-    } finally {
-      bootstrap = null;
-      channel = null;
-      state = State.DISCONNECTED;
+    // STATE STATE STATE
+    public volatile static State state = State.DISCONNECTED;
+    public volatile Channel channel = null;
+    public volatile ConnectionlessBootstrap bootstrap;
+
+    // Configuration
+    public final AtomicLong connectTimeout = new AtomicLong(5000);
+    // Changes to this value are applied only on reconnect.
+    public final AtomicInteger sendBufferSize = new AtomicInteger(16384);
+    public final InetSocketAddress address;
+
+    public UdpTransport(final InetSocketAddress address) {
+        this.address = address;
     }
-  }
 
-  @Override
-  public void reconnect() throws IOException {
-    disconnect();
-    connect();
-  }
+    public UdpTransport(final String host, final int port) throws IOException {
+        this(new InetSocketAddress(host, port));
+    }
 
-  // An Noop
-  @Override
-  public void flush() throws IOException {
-  }
+    public UdpTransport(final String host) throws IOException {
+        this(host, DEFAULT_PORT);
+    }
 
-  @Override
-  public Msg sendRecvMessage(final Msg msg) {
-    throw new UnsupportedOperationException("UDP transport doesn't support receiving messages");
-  }
-
-  @Override
-  public Msg sendMaybeRecvMessage(final Msg msg) {
-    channel.write(msg);
-    return null;
-  }
-
-
-
-
-  public static class DiscardHandler extends SimpleChannelHandler {
-    @Override
-    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
+    public UdpTransport(final int port) throws IOException {
+        this(InetAddress.getLocalHost().getHostAddress(), port);
     }
 
     @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
-      e.getCause().printStackTrace();
-
-      Channel ch = e.getChannel();
-      ch.close();
+    public boolean isConnected() {
+        // Are we in state connected?
+        return state == State.CONNECTED;
     }
-  }
+
+    @Override
+    // Does nothing if not currently disconnected.
+    public synchronized void connect() throws IOException {
+        if (state != State.DISCONNECTED) {
+            return;
+        }
+        ;
+        state = State.CONNECTING;
+
+        // Set up channel factory
+        final ChannelFactory channelFactory = new NioDatagramChannelFactory(
+                Executors.newCachedThreadPool());
+
+        // Create bootstrap
+        bootstrap = new ConnectionlessBootstrap(channelFactory);
+
+        // Reconnect configuration
+        final Timer timer = new HashedWheelTimer();
+        final ChannelHandler udpReconnectHandler = new UdpReconnectHandler(bootstrap, timer);
+        final ChannelHandler timeoutHandler =
+                new WriteTimeoutHandler(timer, WRITE_TIMEOUT);
+
+        // Set up pipeline factory.
+        bootstrap.setPipelineFactory(
+                new ChannelPipelineFactory() {
+                    public ChannelPipeline getPipeline() {
+                        return Channels.pipeline(
+                                pbEncoder, timeoutHandler, udpReconnectHandler, discardHandler
+                        );
+                    }
+                });
+
+        // Set bootstrap options
+        bootstrap.setOption("remoteAddress", address);
+        bootstrap.setOption("sendBufferSize", sendBufferSize.get());
+
+        // Connect
+        final ChannelFuture result = bootstrap.connect().awaitUninterruptibly();
+
+        // Check for errors.
+        if (!result.isSuccess()) {
+            disconnect(true);
+            throw new IOException("Connection failed", result.getCause());
+        }
+
+        // Set up channel
+        channel = result.getChannel();
+        channel.setReadable(false);
+
+        // Done
+        state = State.CONNECTED;
+    }
+
+    @Override
+    public void disconnect() throws IOException {
+        disconnect(false);
+    }
+
+    public synchronized void disconnect(boolean force) throws IOException {
+        if (!(force || state == State.CONNECTED)) {
+            return;
+        }
+
+        try {
+            channel.close().awaitUninterruptibly();
+            bootstrap.releaseExternalResources();
+        } finally {
+            bootstrap = null;
+            channel = null;
+            state = State.DISCONNECTED;
+        }
+    }
+
+    @Override
+    public void reconnect() throws IOException {
+        disconnect();
+        connect();
+    }
+
+    // An Noop
+    @Override
+    public void flush() throws IOException {
+    }
+
+    @Override
+    public Msg sendRecvMessage(final Msg msg) {
+        throw new UnsupportedOperationException("UDP transport doesn't support receiving messages");
+    }
+
+    @Override
+    public Msg sendMaybeRecvMessage(final Msg msg) {
+        channel.write(msg);
+        return null;
+    }
+
+    public static class DiscardHandler extends SimpleChannelHandler {
+        @Override
+        public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+            e.getCause().printStackTrace();
+
+            Channel ch = e.getChannel();
+            ch.close();
+        }
+    }
+
+    public class UdpReconnectHandler extends SimpleChannelUpstreamHandler {
+        final ConnectionlessBootstrap bootstrap;
+        private final Timer timer;
+
+        public UdpReconnectHandler(ConnectionlessBootstrap bootstrap, Timer timer) {
+            this.bootstrap = bootstrap;
+            this.timer = timer;
+        }
+
+        @Override
+        public void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent e) {
+            try {
+                channel.close().awaitUninterruptibly();
+                bootstrap.releaseExternalResources();
+            } finally {
+                channel = null;
+                state = State.DISCONNECTED;
+            }
+        }
+
+        @Override
+        public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e) {
+            timer.newTimeout(new TimerTask() {
+                public void run(Timeout timeout) throws Exception {
+                    state = State.CONNECTING;
+                    // Connect
+                    final ChannelFuture result = bootstrap.connect().awaitUninterruptibly();
+                    // Check for errors.
+                    if (!result.isSuccess()) {
+                        disconnect(true);
+                    }
+                    // Set up channel
+                    channel = result.getChannel();
+                    channel.setReadable(false);
+                    state = State.CONNECTED;
+                }
+            }, RECONNECT_DELAY, TimeUnit.SECONDS);
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+            Throwable cause = e.getCause();
+            cause.printStackTrace();
+            ctx.getChannel().close();
+        }
+    }
 }
